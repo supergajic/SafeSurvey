c9ea136c2b5571260a7d2d867dc0409bdbc1e183
Jimvid <victorjimvid@gmail.com>
2017-05-15 15:47:05 +0200

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,123 @@
+"use strict";
+$('#here').css("display","none");
+$('#try').css("display", "none");
+$('#formbox').css("display","none");
+$('#but_save').css("display", "none");
+if (window.localStorage.getItem("key") > 0) {
+
+	$('.main').css("display","none");
+	$('#formbox').css("display","none");
+	$('#but_save').css("display", "none");
+
+};
+
+if (window.localStorage.getItem("onetime") > 0) {
+	$('#here').css("display","none");
+	$('#try').css("display", "none");
+	$('#formbox').css("display","initial");
+	$('#but_save').css("display", "initial");
+}
+
+if (window.localStorage.getItem("onetime") == null) {
+	$('#here').css("display","initial");
+	$('#try').css("display", "initial");
+}
+
+var loginDB = function() {
+	var username = document.getElementById("user").value;
+	var password = document.getElementById("pw").value;
+
+		// value is now equal to "value"
+	var xhttp = new XMLHttpRequest();
+	var url = "http://appen.sytes.net/scripts/login.php";
+
+	xhttp.open("POST", url, true);
+	xhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+	xhttp.send("usernamePost="+ username + "&passwordPost=" + password);
+
+	xhttp.onreadystatechange = function()
+	{
+		if(xhttp.readyState == 4 && xhttp.status == 200)
+		{
+			var jcontent = JSON.parse(xhttp.response);
+			console.log(jcontent);
+
+
+			if(jcontent.message.includes("Success"))
+			{
+				window.localStorage.setItem("key", jcontent.id);
+				document.getElementById("hejda").innerHTML = "Lokal användare registrerad";
+				$('.main').css("display","none");
+				$('#formbox').css("display","initial");
+				$('#but_save').css("display", "initial");
+				
+
+			}
+			else if(jcontent.includes("Fail"))
+			{
+				// Felaktigt lösenord
+				document.getElementById("hejda").innerHTML = "Kunde ej koppla";
+			}
+			else if(jcontent.includes("NA"))
+			{
+				// Användare finns inte
+				document.getElementById("hejda").innerHTML = "Användare kunde ej hittas";
+			}
+		}
+	};
+};
+
+
+
+function registerDB(username, password)
+{
+	var xhttp = new XMLHttpRequest();
+	var url = "http://appen.sytes.net/scripts/register.php";
+
+	xhttp.open("POST", url, true);
+	xhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+	xhttp.send("usernamePost="+ username + "&passwordPost=" + password);
+
+	xhttp.onreadystatechange = function()
+	{
+		if(xhttp.readyState == 4 && xhttp.status == 200)
+		{
+			var jcontent = JSON.parse(xhttp.response);
+			console.log(jcontent);
+
+			if(jcontent.includes("Exist"))
+			{
+				document.getElementById("hejda").innerHTML = "Användare finns redan";
+
+				// Registrering: Användare finns redan
+			}
+			else if(jcontent.includes("Registered"))
+			{
+				document.getElementById("hejda").innerHTML = "Tack för din registrering, välkommen!";
+
+				// Registrering: Användare är skapad
+			}
+		}
+	};
+}
+
+function checkValue() {
+	var username = document.getElementById("user").value;
+	var password = document.getElementById("pw").value;
+	if(username.length > 0 && password.length > 0){
+		loginDB();
+	}
+}
+
+function regNow() {
+	var username = document.getElementById("user").value;
+	var password = document.getElementById("password").value;
+	var con_password = document.getElementById("con_password").value;
+	if (password == con_password) {
+		registerDB(username, password);
+
+	} else {
+		document.getElementById("hejda").innerHTML = "Du måste skriva in rätt lösenord";
+
+	}
+}
---
+++ js/questions.js
@@ -0,0 +1,89 @@
+var userLocation = "";
+var currentdate = new Date(); 
+var datetime = + currentdate.getFullYear() + "-"  
+			 + ("0" + (currentdate.getMonth()+1)).slice(-2)  + "-" 
+ 			 + ("0" + currentdate.getDate()).slice(-2)  + " "  
+             + ("0" + currentdate.getHours()).slice(-2) + ":"  
+             + ("0" + currentdate.getMinutes()).slice(-2) + ":"
+             + ("0" + currentdate.getSeconds()).slice(-2)
+
+navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
+
+  function foundLocation(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    userLocation = lat +',' + lon;
+
+  }
+  
+  function noLocation() {
+    console.log("no geo hereee");
+  }
+
+var survey = [];
+var count = 0;
+var q = [];
+var questions = {};
+var counterTwo = 0;
+var chosenAnswer ="";
+var dates = "";
+var quest = document.getElementsByName('quest');
+var userId = window.localStorage.getItem("key");
+
+
+var getajax = function () {
+
+$.ajax({url: "http://appen.sytes.net/scripts/getsurvey.php", success: function(result){
+        survey = JSON.parse(result);
+    }});
+	
+	setTimeout(quests,2500);
+}
+
+var quests = function () {
+	removeChildren('formbox');
+	if(survey.question.length > count) {
+	counterTwo = 0;
+	$('#formbox').append("<div class='questionz'><h2>" + survey.question[count] + "</h2></div>");
+	$('#formbox').append("<div id='q'></div>");
+
+	for(j = 0; j < survey.answers[count].length; j++) {
+		
+		// console.log("counterTwo:" + counterTwo);
+		$('#q').append('<div class="alternative"><input class="answers" type="radio" name="quest" value="'+ counterTwo +'">' + "<label for='answers'>" + survey.answers[count][j] + "</label>" + "</div>");
+		counterTwo = counterTwo + 1;
+	 	}	
+	 } else if (survey.question.length = count) {
+	 	console.log("send json");
+	 	var myJsonString = JSON.stringify(q);
+		var xhttp = new XMLHttpRequest();
+		var url = "http://appen.sytes.net/scripts/sendsurvey.php";
+
+		xhttp.open("POST", url, true);
+		xhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+		xhttp.send("esmsurvey=" + myJsonString);
+
+	 	alert("thanks! --> next stuff");
+	 	$('#but_save').css("display", "none");
+	 }
+};
+
+var complete = false;
+var answerss = function() {
+	number = count + 1;
+	for(var i = 0; i < quest.length; i++){
+			if (quest[i].checked) {
+ 		 	  chosenAnswer = quest[i].value;
+			  q.push({'question':count + 1, 'answer': number + chosenAnswer, 'timeinput': datetime, 'userid': userId, 'geoLocation' : userLocation});
+			  complete = true;
+ 			} 
+		}
+		if(complete) $(window).ready(quests);
+
+};
+
+$(window).ready(getajax);
+$('#but_save').click(answerss);
+$('#but_save').click(function(){count = count + 1; })
+
+var myJsonString = JSON.stringify(q);
